b90ab2440764d467aa02c1e9f4cc4dfefb46077f
Sahar Ramezani (Alba) <122964803+SaharRamezani@users.noreply.github.com>
2025-05-05 15:51:27 +0200

    Add files via upload

--- sketch_may5a/sketch_may5a.ino
+++ sketch_may5a/sketch_may5a.ino
@@ -18,15 +18,27 @@ void setup() {
 
 void loop() {
   float temperature = sensor.getTemperature(1);  // 1 = Celsius (this works per earlier error)
-  float humidity = sensor.getHumidity();         // works with 0 arguments
+  float humidity = sensor.getHumidity();   
 
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println(" °C");
-
   Serial.print("Humidity: ");
   Serial.print(humidity);
   Serial.println(" %");
+  Serial.print("Ultraviolet Intensity: ");
+  Serial.print(sensor.getUltravioletIntensity());
+  Serial.println(" mw/cm2");
+  Serial.print("LuminousIntensity: ");
+  Serial.print(sensor.getLuminousIntensity());
+  Serial.println(" lx");
+  Serial.print("Atmospheric pressure: ");
+  Serial.print(sensor.getAtmospherePressure(HPA));
+  Serial.println(" hpa");
+  Serial.print("Altitude: ");
+  Serial.print(sensor.getElevation());
+  Serial.println(" m");
+  Serial.println("-------------------------------");
 
   delay(2000);
 }
